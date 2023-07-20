558ce68b10350f867d9df305c58a4e15c076e846
MZT-Venom <81874355+MZT-Venom@users.noreply.github.com>
2023-07-20 23:05:22 +0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styles from './App.module.css'; // CSS module for styling
 
-function App() {
+function Form() {
+  const [backgroundColor, setBackgroundColor] = useState('#f1f1f1');
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    contact: '',
+    email: '',
+    description: '',
+    color: '#f1f1f1',
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleColorChange = (event) => {
+    const { value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      color: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setBackgroundColor(formData.color);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.screen} style={{ backgroundColor }}>
+      <h1 className={styles.title}>Form</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <label className={styles.label} htmlFor="firstName">
+          First Name:
+        </label>
+        <input
+          className={styles.input}
+          type="text"
+          id="firstName"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleInputChange}
+          required
+        />
+
+        <label className={styles.label} htmlFor="lastName">
+          Last Name:
+        </label>
+        <input
+          className={styles.input}
+          type="text"
+          id="lastName"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleInputChange}
+          required
+        />
+
+        <label className={styles.label} htmlFor="contact">
+          Contact:
+        </label>
+        <input
+          className={styles.input}
+          type="tel"
+          id="contact"
+          name="contact"
+          value={formData.contact}
+          onChange={handleInputChange}
+          required
+        />
+
+        <label className={styles.label} htmlFor="email">
+          Email:
+        </label>
+        <input
+          className={styles.input}
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+          required
+        />
+
+        <label className={styles.label} htmlFor="description">
+          Description:
+        </label>
+        <textarea
+          className={styles.textarea}
+          id="description"
+          name="description"
+          value={formData.description}
+          onChange={handleInputChange}
+          required
+        />
+
+        <label className={styles.label} htmlFor="color">
+          Color:
+        </label>
+        <input
+          className={styles.colorPicker}
+          type="color"
+          id="color"
+          name="color"
+          value={formData.color}
+          onChange={handleColorChange}
+          required
+        />
+
+        <button className={styles.submitButton} type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
 
-export default App;
+export default Form;
